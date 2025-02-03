990cbddd9c5d2457c23d1783701994c384347c70
Luis Rodriguez <luisrocedev@gmail.com>
2025-02-03 14:49:28 +0100

    1

--- admin/config/db_connect.php
+++ admin/config/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "82.165.43.137";  // Servidor de la base de datos (ajusta si usas otro)
+$servername = "localhost";  // Servidor de la base de datos (ajusta si usas otro)
 $username = "taronjaboxvalencia";         // Usuario de MySQL
 $password = "taronjaboxvalencia";             // Contraseña de MySQL (déjala vacía si no tiene)
 $database = "taronjaboxvalencia"; // Nombre de la BD (verifica que sea correcto)
